18c497186e7b5fe673ec30b7c0928e4dd5d122c4
Jorge <jmotosp@gmail.com>
2026-02-17 20:00:15 +0100

    Load active game when refresh board page

--- app/webapp/controller/Board.controller.js
+++ app/webapp/controller/Board.controller.js
@@ -1,13 +1,19 @@
 sap.ui.define([
-	"./BaseController", "sap/m/MessageBox"
-], function (BaseController, MessageBox) {
+	"./BaseController", "sap/m/MessageBox", "sap/ui/model/json/JSONModel", "../model/Game", "../model/Repository"
+], function (BaseController, MessageBox, JSONModel, Game, Repository) {
 	"use strict";
 
 	const gameModelName = "game";
 
 	return BaseController.extend("com.trifork.tictactoe.controller.Game", {
 		onInit: function () {
-			// Initialize if needed
+			const gameModel = this._getGameModel();
+
+			if (gameModel?.getData()) {
+				return;
+			}
+
+			this._loadActiveGame();
 		},
 
 		onCellPress: function (event, row, column) {
@@ -22,6 +28,28 @@ sap.ui.define([
 				MessageBox.error(error.message);
 				this._setBusy(false);
 			}.bind(this));
+		},
+
+		_loadActiveGame: function() {
+			const mainModel = this.getOwnerComponent().getModel();
+			const repository = new Repository(mainModel);
+			const game = new Game(repository);
+
+			this._setBusy(true);
+			game.loadActiveGame().then(function (activeGame) {
+				this.getOwnerComponent().setModel(new JSONModel(activeGame), gameModelName);
+				this._setBusy(false);
+			}.bind(this)).catch(function (error) {
+				MessageBox.error(this._getText("msgErrorLoadingGame", [error.message]));
+				this._setBusy(false);
+			}.bind(this));
+		},
+
+		_getText: function (key, args) {
+			if (!this.i18n) {
+				this.i18n = this.getView().getModel("i18n").getResourceBundle();
+			}
+			return this.i18n.getText(key, args);
 		},
 
 		_getGameModel: function() {
